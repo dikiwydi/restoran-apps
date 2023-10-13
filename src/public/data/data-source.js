adc39435bd6f206874a4c946be96ceed525800de
dikiwydi <dikiwahyudi268@gmail.com>
2023-10-13 13:35:41 +0700

    init

---
+++ src/public/data/data-source.js
@@ -0,0 +1,19 @@
+import data from './DATA.json';
+
+let makeCard = '';
+
+for (let i = 0; i < data.restaurants.length; i++) {
+  makeCard += `
+   <article class="post-item">
+    <figure class="post-item__image">
+      <img class="post-item__thumbnail" src="${data.restaurants[i].pictureId}" alt="" />
+      <figcaption class="post-item__city">${data.restaurants[i].city}</figcaption>
+    </figure>
+    <div class="post-item__info">
+      <h2 class="post-item__name">${data.restaurants[i].name}</h2>
+      <p class="post-item__rating">${data.restaurants[i].rating}</p>
+      <p class="post-item__description">${data.restaurants[i].description}</p>
+    </div>
+  </article>`;
+}
+document.querySelector('.posts').innerHTML += makeCard;
---
+++ src/scripts/index.js
@@ -0,0 +1,5 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css';
+import '../public/data/data-source';
+
+console.log('Hello Coders! :)');
